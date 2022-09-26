8affb713fc388ed4beafd8c9e0db4d7939b6cb80
Claire Steinhoff <claire.steinhoff@gmail.com>
2022-09-26 11:48:10 -0600

    commit tree 311a6be0012eb6a9d22e30243ac65122a29e882e

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+// Add your code hered
+function submitData(formName, formEmail) {
+    const formData = {name: formName, email: formEmail}
+    return fetch('http://localhost:3000/users',
+    {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept" : "application/json"
+        },
+         body: JSON.stringify(formData)
+    }
+    )
+    .then(res => res.json())
+    .then(function (obj) {document.body.innerHTML = obj["id"]})
+    .catch(function (e) {document.body.innerHTML = e.message})
+    
+}
+
+function addError(info) {
+    document.body.innerHTML = info.message;
+}
+
+function addToPage(info) {
+    const idValue = document.createElement('p')
+    idValue.textContent = info;
+    document.body.appendChild(idValue)
+}
+
+submitData("New Name!", "someemail@hotmail.com")
